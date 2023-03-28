ed18f5773f0783549d8c0c93c792118d2d03145b
wilpola <wilpolaproduction@gmail.com>
2023-03-28 17:50:23 +0300

    Testing rollup build

---
+++ dist/esm/types/components/Input/Input.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+type Props = {
+    type?: string;
+    class?: string;
+    id?: string;
+};
+declare const Input: (params: Props) => JSX.Element;
+export default Input;
